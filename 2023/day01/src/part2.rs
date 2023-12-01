40cf3ad6c3e3708fdd49a883cfccebac500a0880
Teemu Pätsi <teemu@paddy.fi>
2023-12-01 14:35:24 +0200

    Resolved Day01 part2

---
+++ 2023/day01/src/part2.rs
@@ -0,0 +1,57 @@
+use phf::phf_map;
+static SPELLED_NUMBERS: phf::Map<&'static str, u32> = phf_map! {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+};
+
+pub fn solve(input: &str) -> String {
+    let result = input
+        .lines()
+        .map(combine_first_and_last_number)
+        .sum::<u32>();
+
+    result.to_string()
+}
+
+fn combine_first_and_last_number(input: &str) -> u32 {
+    let mut numbers = (0..input.len()).filter_map(|i| {
+        let remaining_line = &input[i..];
+        for (key, value) in SPELLED_NUMBERS.entries() {
+            if remaining_line.starts_with(key) {
+                return Some(value.to_owned());
+            }
+        }
+        remaining_line.chars().next().unwrap().to_digit(10)
+    });
+
+    let first = numbers.next().unwrap();
+    match numbers.last() {
+        Some(number) => first * 10 + number,
+        None => first * 10 + first,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let solution = "281";
+        assert_eq!(solution, solve(input));
+    }
+}
