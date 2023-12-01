ebf14d6a726fbdb3caf54a3eb3a8c842481f8062
Teemu Pätsi <teemu@paddy.fi>
2023-12-01 14:39:26 +0200

    Use array of tuples instead of phf::Map

--- 2023/day01/src/part2.rs
+++ 2023/day01/src/part2.rs
@@ -1,15 +1,14 @@
-use phf::phf_map;
-static SPELLED_NUMBERS: phf::Map<&'static str, u32> = phf_map! {
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "four" => 4,
-    "five" => 5,
-    "six" => 6,
-    "seven" => 7,
-    "eight" => 8,
-    "nine" => 9,
-};
+static SPELLED_NUMBERS: &[(&str, u32)] = &[
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
 
 pub fn solve(input: &str) -> String {
     let result = input
@@ -23,7 +22,7 @@ pub fn solve(input: &str) -> String {
 fn combine_first_and_last_number(input: &str) -> u32 {
     let mut numbers = (0..input.len()).filter_map(|i| {
         let remaining_line = &input[i..];
-        for (key, value) in SPELLED_NUMBERS.entries() {
+        for (key, value) in SPELLED_NUMBERS {
             if remaining_line.starts_with(key) {
                 return Some(value.to_owned());
             }
