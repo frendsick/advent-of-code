11586091c1eeb4bd24ce03221c3f54173ef85ffd
Teemu Pätsi <teemu@paddy.fi>
2023-12-01 12:18:20 +0200

    Resolved Day01 part1

---
+++ 2023/day01/src/part1.rs
@@ -0,0 +1,44 @@
+pub fn solve(input: &str) -> String {
+    let result = input
+        .lines()
+        .map(combine_first_and_last_number)
+        .sum::<u32>();
+
+    result.to_string()
+}
+
+fn combine_first_and_last_number(input: &str) -> u32 {
+    let mut first: Option<u32> = None;
+    let mut last: Option<u32> = None;
+
+    for c in input.chars() {
+        if !c.is_ascii_digit() {
+            continue;
+        }
+
+        let digit = c.to_digit(10).unwrap();
+
+        if first.is_none() {
+            first = Some(digit);
+        }
+
+        last = Some(digit);
+    }
+
+    first.unwrap() * 10 + last.unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let solution = "142";
+        assert_eq!(solution, solve(input));
+    }
+}
