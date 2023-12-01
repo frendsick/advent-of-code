2821c8592a08242d8f9ff4a54fb8820c59d30152
Teemu Pätsi <teemu@paddy.fi>
2023-12-01 11:27:25 +0200

    Template for each day

---
+++ 2023/template/src/bin/part1.rs
@@ -0,0 +1,7 @@
+use day00::part1::process;
+
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let result = solve(input);
+    println!("{}", result);
+}
---
+++ 2023/template/src/part2.rs
@@ -0,0 +1,15 @@
+pub fn solve(_input: &str) -> String {
+    todo!("Day XX - Part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "input";
+        let solution = "solution";
+        assert_eq!(solution, solve(input));
+    }
+}
