122c95906e6e1177b5d080bd95ed4e97387af911
Teemu Pätsi <teemu@paddy.fi>
2023-12-05 01:19:56 +0200

    Refactor using functional programming

--- 2023/day02/src/part1.rs
+++ 2023/day02/src/part1.rs
@@ -19,39 +19,33 @@ fn parse_possible_game_id(input: &str) -> Option<u32> {
     }
 }
 
-fn is_possible_game(input: &str) -> bool {
-    for part in input.split(&[':', ',', ';']) {
-        // Skip over "Game X"
-        if part.starts_with("Game") {
-            continue;
-        }
-
-        // Parse the color and the amount of cubes
-        let mut color_with_count = part.split_whitespace();
-        let cube_amount: u32 = color_with_count
-            .next()
-            .expect("number of colored cubes")
-            .parse()
-            .expect("valid number");
-        let cube_color: &str = color_with_count.next().expect("color");
-
-        // The game is not possible if elf has more colored cubes than available in the bag
-        // Expect that the color exists in the bag
-        if BAG.get(cube_color).unwrap() < &cube_amount {
-            return false;
-        }
-    }
-    true
-}
-
 fn parse_game_id(input: &str) -> u32 {
-    // The game ID starts after "GAME " and ends at ':'
     let start_ptr: usize = 5;
     let end_ptr: usize = input.find(':').expect("game ID should end with ':'");
-    let game_id: u32 = input[start_ptr..end_ptr]
+    input[start_ptr..end_ptr]
         .parse()
-        .expect("game ID should be a number");
-    game_id
+        .expect("game ID should be a number")
+}
+
+fn is_possible_game(input: &str) -> bool {
+    input
+        // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        .split(&[':', ',', ';'])
+        // Skip over "Game X"
+        .filter(|part| !part.starts_with("Game"))
+        // Game is possible if all sets of cubes are within the bag
+        .all(is_possible_set)
+}
+
+fn is_possible_set(game_set: &str) -> bool {
+    let mut color_with_count = game_set.split_whitespace();
+    let cube_amount: u32 = color_with_count
+        .next()
+        .expect("number of colored cubes")
+        .parse()
+        .expect("valid number");
+    let cube_color: &str = color_with_count.next().expect("color");
+    BAG.get(cube_color).unwrap() >= &cube_amount
 }
 
 #[cfg(test)]
