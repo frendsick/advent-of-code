eedb7c4a428d5ba297aa11f74c174726d7e3f309
Teemu Pätsi <teemu@paddy.fi>
2023-12-05 19:56:42 +0200

    Resolve Day05 part2
    Iterate possible locations from 0 and find the first location which
    is a valid seed when the backwards ConversionMaps are applied in reverse

--- 2023/day05/src/part2.rs
+++ 2023/day05/src/part2.rs
@@ -14,22 +14,32 @@ pub fn solve(input: &str) -> String {
     let seeds = parse_seeds(lines.next().expect("seed line"));
     lines.next(); // Skip empty line after seeds
 
-    let conversion_maps_container: Vec<Vec<ConversionMap>> = generate_conversion_maps(&mut lines);
+    // Parse conversion maps and get the conversion rules reversed
+    let conversion_maps_container: Vec<Vec<ConversionMap>> =
+        generate_backwards_conversion_maps(&mut lines);
 
-    // Apply all maps to convert from seeds to locations
-    let locations: Vec<u32> = conversion_maps_container
-        .iter()
-        .fold(seeds, |seeds, map| apply_map(&seeds, map));
+    // Find the smallest location that has a valid seed
+    let mut location = 0;
+    loop {
+        let possible_seed = conversion_maps_container
+            .iter()
+            .fold(location, |acc, map| apply_map_to_number(acc, map));
+        if is_valid_seed(possible_seed, &seeds) {
+            break;
+        }
+        location += 1;
+    }
 
-    // Return the lowest location number
-    locations
-        .iter()
-        .min()
-        .expect("lowest location number")
-        .to_string()
+    location.to_string()
 }
 
-fn generate_conversion_maps(lines: &mut Lines) -> Vec<Vec<ConversionMap>> {
+fn is_valid_seed(seed: u32, seeds: &[u32]) -> bool {
+    seeds
+        .chunks_exact(2)
+        .any(|chunk| chunk[0] <= seed && seed < chunk[0] + chunk[1])
+}
+
+fn generate_backwards_conversion_maps(lines: &mut Lines) -> Vec<Vec<ConversionMap>> {
     let conversion_categories = [
         "seed-to-soil",
         "soil-to-fertilizer",
@@ -40,38 +50,29 @@ fn generate_conversion_maps(lines: &mut Lines) -> Vec<Vec<ConversionMap>> {
         "humidity-to-location",
     ];
 
-    // Parse conversion maps for each category
-    conversion_categories
+    // Parse backwards conversion maps for each category
+    let mut conversion_maps: Vec<Vec<ConversionMap>> = conversion_categories
         .iter()
         .map(|_| {
             lines.next(); // Skip category header
-            parse_conversion_maps(lines)
+            parse_backwards_conversion_maps(lines)
         })
-        .collect()
+        .collect();
+
+    // Reverse the order so it is easier to apply the conversion map rules in the reverse order
+    conversion_maps.reverse();
+    conversion_maps
 }
 
 fn parse_seeds(line: &str) -> Vec<u32> {
     let numbers_section = line.split(':').last().expect("seeds");
-    let numbers: Vec<u32> = parse_numbers_separated_by_spaces(numbers_section);
-
-    numbers
-        .chunks_exact(2)
-        .map(|chunk| chunk[0]..chunk[0] + chunk[1])
-        .flatten()
-        .collect()
+    parse_numbers_separated_by_spaces(numbers_section)
 }
 
-fn parse_conversion_maps(lines: &mut Lines) -> Vec<ConversionMap> {
+fn parse_backwards_conversion_maps(lines: &mut Lines) -> Vec<ConversionMap> {
     lines
         .take_while(|line| !line.is_empty())
-        .map(parse_conversion_map)
-        .collect()
-}
-
-fn apply_map(input_numbers: &[u32], map: &[ConversionMap]) -> Vec<u32> {
-    input_numbers
-        .iter()
-        .map(|&number| apply_map_to_number(number, map))
+        .map(parse_backwards_conversion_map)
         .collect()
 }
 
@@ -87,12 +88,14 @@ fn is_number_in_map(number: usize, map: &ConversionMap) -> bool {
     map.source_start <= number && number < map.source_start + map.length
 }
 
-fn parse_conversion_map(line: &str) -> ConversionMap {
+fn parse_backwards_conversion_map(line: &str) -> ConversionMap {
     let numbers = parse_numbers_separated_by_spaces(line);
     assert_eq!(numbers.len(), 3, "ConversionMap definition");
+
+    // Destination and source are swapped to allow going backwards
     ConversionMap {
-        destination_start: numbers[0] as usize,
-        source_start: numbers[1] as usize,
+        destination_start: numbers[1] as usize,
+        source_start: numbers[0] as usize,
         length: numbers[2] as usize,
     }
 }
